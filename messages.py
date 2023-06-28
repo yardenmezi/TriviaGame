41b69bf6701930faeb10861150a6c173a616f191
yadenmezi <Yarden.Mezi@mail.huji.ac.il>
2023-06-28 12:14:37 +0300

    clean method - check if question is available when validate input. printing msg to the user is done in the question asking flow.

--- messages.py
+++ messages.py
@@ -2,5 +2,4 @@ GAME_OVER = "Game over"
 QUESTION_CHOSEN = "You already chose this question"
 USER_INPUT_INVALID = "Input is not valid"
 TRIVIA_UNAVAILABLE = "Sorry, trivia is not currently available"
-
-
+CHOOSING_QUESTION = 'please choose an available category'
