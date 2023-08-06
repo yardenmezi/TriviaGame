624ee359c08907c4b15f73781aa876f2fca238d8
yadenmezi <Yarden.Mezi@mail.huji.ac.il>
2023-08-06 14:47:39 +0300

    fix pr suggestions

--- trivia_api.py
+++ trivia_api.py
@@ -41,3 +41,4 @@ def get_trivia_categories(categories_number: int) -> list:
     chosen_indices = random.sample(range(min(len(all_possible_categories), categories_number)), categories_number)
     categories = [all_possible_categories[i] for i in chosen_indices]
     return categories
+
