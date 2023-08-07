903d04c714d04813fc39c9e9cdb8b25c3b135f5f
yadenmezi <Yarden.Mezi@mail.huji.ac.il>
2023-08-07 15:13:11 +0300

    fix pr suggestions

--- game.py
+++ game.py
@@ -18,18 +18,11 @@ class Game:
     def __init__(self, formatter, game_config: dict):
         self._score = 0
         self._formatter = formatter
+        self._questions_api_config = game_config["QUESTIONS_NUMBER"]
         self._trivia_categories: list = trivia_api.get_trivia_categories(game_config["QUESTIONS_NUMBER"])
         self._available_category_indices: set = {i for i in range(0, len(self._trivia_categories))}
 
     offset_user_input = lambda x: x - 1
-
-    def start(self) -> int:
-        while not self._is_game_over():
-            self._manage_round_flow()
-        return self._score
-
-    def _is_game_over(self) -> bool:
-        return len(self._available_category_indices) == 0
 
     def _handle_question_asking_flow(self, question_idx):
         question: TriviaQuestion = trivia_api.get_trivia_question(self._trivia_categories[question_idx]["id"])
@@ -47,3 +40,11 @@ class Game:
         self._formatter.display_game_table(self._trivia_categories, self._available_category_indices)
         _question_idx = get_valid_user_input(self._available_category_indices, messages.CHOOSING_QUESTION)
         self._handle_question_asking_flow(_question_idx)
+
+    def _is_game_over(self) -> bool:
+        return len(self._available_category_indices) == 0
+
+    def start(self) -> int:
+        while not self._is_game_over():
+            self._manage_round_flow()
+        return self._score
