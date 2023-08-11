cd953ee2f4d308cd6ea0aeb7e1490b39269600af
yadenmezi <Yarden.Mezi@mail.huji.ac.il>
2023-08-11 23:45:01 +0300

    Capital letter

--- messages.py
+++ messages.py
@@ -2,6 +2,6 @@ GAME_OVER = "Game over"
 QUESTION_CHOSEN = "You already chose this question"
 USER_INPUT_INVALID = "Input is not valid"
 TRIVIA_UNAVAILABLE = "Sorry, trivia is not currently available."
-CHOOSING_QUESTION = 'please choose an available category'
+CHOOSING_QUESTION = 'Please choose an available category'
 CONNECTION_ERROR = "In case you're facing connectivity problems, " \
                    "please fix your internet connection and give it another try."
